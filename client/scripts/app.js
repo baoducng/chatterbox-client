3d6f20113928f617e8bcb3eedc5db169ec72eeb9
Hack Reactor Students <accounts@hackreactor.com>
2014-06-16 15:35:13 -0700

    Update

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,80 @@
-// YOUR CODE HERE:
 
+// SELECT VARIABLES as DOM NODES
+var $input = $('#createMsgInput');
+var $send = $('#createMsgButton');
+
+// YOUR CODE HERE:
+window.URL = "https://api.parse.com/1/classes/chatterbox";
+
+var app = {
+  server : 'https://api.parse.com/1/classes/chatterbox',
+  init: function(){
+    //add click event listener to the message button
+    $send.on('click', function(){
+    });
+
+  },
+  send: function(data){
+    $.ajax({
+      url : URL,
+      type: 'POST',
+      data: JSON.stringify(data),
+    });
+  },
+  fetch: function(){
+    $.ajax({
+      type: 'GET',
+      url : app.server,
+      contentType: 'application/json',
+      sucess: function(data){
+        console.log(data);
+      },
+      error: function(data){
+        console.log(data);
+      }
+    });
+  }
+};
+
+app.init();
+app.fetch();
+//app.fetch();
+
+//console.log("yolo")
+
+
+
+
+
+
+
+
+
+/*
+
+var Server = function(){
+  return {
+    create: function(data){
+      return $.ajax({
+        url : URL,
+        type: 'POST',
+        data: data
+      });
+    },
+    get: function(){
+      return $.ajax({
+        url : URL,
+        type: 'GET',
+      });
+    },
+    update: function(){
+
+    },
+    destroy: function(){
+
+    }
+  };
+};
+
+
+ */
