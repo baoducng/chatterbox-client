00ac83c82f5b3704433e5f5e0b925ac3c196221d
Hack Reactor Students <accounts@hackreactor.com>
2014-06-16 20:01:58 -0700

    Complete Basic REquirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,80 +1,133 @@
+//
 
-// SELECT VARIABLES as DOM NODES
-var $input = $('#createMsgInput');
-var $send = $('#createMsgButton');
 
-// YOUR CODE HERE:
-window.URL = "https://api.parse.com/1/classes/chatterbox";
+  // SELECT VARIABLES as DOM NODES
 
-var app = {
-  server : 'https://api.parse.com/1/classes/chatterbox',
-  init: function(){
-    //add click event listener to the message button
-    $send.on('click', function(){
-    });
-
-  },
-  send: function(data){
-    $.ajax({
-      url : URL,
-      type: 'POST',
-      data: JSON.stringify(data),
-    });
-  },
-  fetch: function(){
-    $.ajax({
-      type: 'GET',
-      url : app.server,
-      contentType: 'application/json',
-      sucess: function(data){
-        console.log(data);
-      },
-      error: function(data){
-        console.log(data);
+  window.URL = "https://api.parse.com/1/classes/chatterbox";
+  window.currentRoom = undefined;
+  var Rooms = function(data){
+    var object = {};
+    //take out all the duplicates.
+    for(var i = 0; i < data.length; i++){
+      if(!object[data[i].roomname]){
+        object[data[i].roomname] = data[i].roomname;
       }
-    });
-  }
-};
-
-app.init();
-app.fetch();
-//app.fetch();
-
-//console.log("yolo")
+    }
+    //result will be an array of all the rooms.
+    return object;
+  };
 
 
+  var makeRoom = function( room ){
+    return $('<li></li>').addClass('list-group-item').text(room);
+  };
+
+  // YOUR CODE HERE:
+
+  var app = {
+    messages: {},
+    server : 'https://api.parse.com/1/classes/chatterbox',
+    init: function(){
+      var $input = $('#createMsgInput');
+      var $send = $('#createMsgButton');
+      var cRoom = "";
+
+      //add click event listener to the message button
+      $send.on('click', function(){
+        var message = {
+          text : $input.val(),
+          username : 'Mr Sexy',
+          roomname: cRoom
+        };
+        app.send(message);
+        //console.log($input.val());
+      });
+
+      $("#roomsList").on("click", "li", function(){
+        var r = $(this).text();
+        cRoom = r;
+
+        $("#roomsList li").removeClass("bg-blue");
+        $("#messagesList li").hide();
+        $(this).addClass("bg-blue");
+        $("#messagesList").find("." + r).show();
+      });
 
 
-
-
-
-
-
-/*
-
-var Server = function(){
-  return {
-    create: function(data){
-      return $.ajax({
+    },
+    send: function(data){
+      console.log(data);
+      $.ajax({
         url : URL,
-        type: 'POST',
-        data: data
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: function( data ){
+        }
       });
     },
-    get: function(){
-      return $.ajax({
-        url : URL,
-        type: 'GET',
+    fetch: function(){
+      var date  = new Date();
+      $.ajax({
+        type: "GET",
+        url : app.server,
+        data: { order: "-createdAt"},
+        contentType: "application/json",
+        success: function(data){
+          $("#roomsList").html("");//empties the roomlist
+          $("#messagesList").html("");
+
+          var rooms = Rooms(data.results);
+
+          _.each(rooms, function (item) {//go through all the keys
+            var node = makeRoom(item);//make a room
+            $('#roomsList').append(node);//add it to room list
+          });
+          _.each(data.results, function(item){
+            var $usermsg = $('<div></div>').addClass('list-group-item-text').html('<strong>'+item.text+'</strong>');
+            var $username = $('<div></div>').addClass('list-group-item-text').html('<small>'+item.username+'</small>');
+            var $node = $('<li></li>').addClass('list-group-item').addClass(item.roomname);
+            $node.append($usermsg).append($username);
+            $('#messagesList').prepend($node);
+            app.messages[item.objectId] = item;
+          });
+        },
+        error: function(data){
+          console.log('fail',data);
+        }
       });
-    },
-    update: function(){
-
-    },
-    destroy: function(){
-
     }
   };
-};
+$(document).ready(function(){
 
 
+
+  app.init();
+  app.fetch();
+  $('#refresh').on('click', function(){
+    app.fetch();
+  });
+
+  setInterval(function(){
+    app.destroy();
+  }, 5000);
+});
+
+/*
+createdAt: "2013-10-07T16:22:03.280Z"
+objectId: "teDOY3Rnpe"
+roomname: "lobby"
+text: "hello"
+updatedAt: "2013-10-07T16:22:03.280Z"
+username: "gary"
  */
+
+
+
+/*<html>
+Latest comment:
+<script>
+  delete document.body
+</script>
+</html>
+*/
